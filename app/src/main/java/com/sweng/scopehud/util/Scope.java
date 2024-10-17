390683821bb8797aeeb9ddd21ca29ba0d6677c86
André Medeiros <andremedeiros1998@gmail.com>
2024-10-16 20:21:15 -0700

    [add] ScopeRecyclerViewAdapter class

--- app/src/main/java/com/sweng/scopehud/util/Scope.java
+++ app/src/main/java/com/sweng/scopehud/util/Scope.java
@@ -1,2 +1,25 @@
-package com.sweng.scopehud.util;public class Scope {
+package com.sweng.scopehud.util;
+
+import android.location.Location;
+
+import java.util.Date;
+
+//Scopes are stored in the database
+public class Scope {
+    private int id; //database row id
+    private String name; //user-configurable scope name
+    private String brand; //brand of scope
+    private Date zeroDate; //date on which the scope was last zeroed
+    private Location zeroLocation; //location at which the scope was last zeroed
+    private float maxMagnification; //maximum magnification of scope (1 for non magnified scopes)
+    private boolean variableMagnification; //whether the scope has variable magnification
+
+
+    public Scope() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
 }
