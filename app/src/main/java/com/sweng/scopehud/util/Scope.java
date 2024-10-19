6a64e579f5f94a433ca7c332a67d34332af1df53
André Medeiros <andremedeiros1998@gmail.com>
2024-10-19 12:10:13 -0700

    [create] dbHandler and make scope parcelable

--- app/src/main/java/com/sweng/scopehud/util/Scope.java
+++ app/src/main/java/com/sweng/scopehud/util/Scope.java
@@ -1,11 +1,15 @@
 package com.sweng.scopehud.util;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 
 import java.util.Date;
 
 //Scopes are stored in the database
-public class Scope {
+public class Scope implements Parcelable {
     private int id; //database row id
     private String name; //user-configurable scope name
     private String brand; //brand of scope
@@ -13,12 +17,54 @@ public class Scope {
     private boolean variableMagnification; //whether the scope has variable magnification
     private ScopeZero scopeZero; //last scope zero data
 
-
-    public Scope() {
-
-    }
-
     public String getName() {
         return name;
+    }
+
+    public Scope(int id, String name, String brand, float maxMagnification,
+                 boolean variableMagnification, ScopeZero scopeZero) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.maxMagnification = maxMagnification;
+        this.variableMagnification = variableMagnification;
+        this.scopeZero = scopeZero;
+    }
+
+    protected Scope(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        brand = in.readString();
+        maxMagnification = in.readFloat();
+        variableMagnification = in.readByte() != 0;
+        scopeZero = in.readParcelable(ScopeZero.class.getClassLoader());
+    }
+
+    public static final Creator<Scope> CREATOR = new Creator<Scope>() {
+        @Override
+        public Scope createFromParcel(Parcel in) {
+            return new Scope(in);
+        }
+
+        @Override
+        public Scope[] newArray(int size) {
+            return new Scope[size];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(name);
+        parcel.writeString(brand);
+        parcel.writeFloat(maxMagnification);
+        parcel.writeByte((byte) (variableMagnification ? 1 : 0));
+        parcel.writeParcelable(scopeZero, i);
     }
 }
--- app/src/main/java/com/sweng/scopehud/util/ScopeZero.java
+++ app/src/main/java/com/sweng/scopehud/util/ScopeZero.java
@@ -1,21 +1,53 @@
 package com.sweng.scopehud.util;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 
 import java.util.Date;
 
-public class ScopeZero {
+public class ScopeZero implements Parcelable {
     private int distance;
-    private int windage;
-    private int elevation;
+    private float windage;
+    private float elevation;
     private Date date;
-    private Location location;
 
-    public ScopeZero(int distance, int windage, int elevation, Date date, Location location) {
+    public ScopeZero(int distance, float windage, float elevation, Date date) {
         this.distance = distance;
         this.windage = windage;
         this.elevation = elevation;
         this.date = date;
-        this.location = location;
+    }
+
+    protected ScopeZero(Parcel in) {
+        distance = in.readInt();
+        windage = in.readInt();
+        elevation = in.readInt();
+    }
+
+    public static final Creator<ScopeZero> CREATOR = new Creator<ScopeZero>() {
+        @Override
+        public ScopeZero createFromParcel(Parcel in) {
+            return new ScopeZero(in);
+        }
+
+        @Override
+        public ScopeZero[] newArray(int size) {
+            return new ScopeZero[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeInt(distance);
+        parcel.writeFloat(windage);
+        parcel.writeFloat(elevation);
     }
 }
